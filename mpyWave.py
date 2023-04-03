70929c2eceba10f0f99b860a3477f58810d6421c
Andrea Vaccaro <100683661+ANDRVV@users.noreply.github.com>
2023-04-03 23:52:35 +0200

    Update mpyWave.py

--- mpyWave.py
+++ mpyWave.py
@@ -1,110 +1,50 @@
-from machine import Pin
+from machine import Pin, UART
 import time
 
 BIT_00 = str(0x0)
 BIT_01 = str(0x1)
 
 NO_TIMEOUT = 0x6e6f74696d65
-DELAY_OPERATOR = 0xf4240
-
-SYNC_HIGH = 1
-SYNC_LOW = 31
-
-PULSE_HIGH_BIT00 = 1
-PULSE_LOW_BIT00 = 3
-PULSE_HIGH_BIT01 = 3
-PULSE_LOW_BIT01 = 1
 
 TX = 0x1d36
 RX = 0x1c6e
 
-PROTOCOL_01 = [350, 1, 31, 1, 3, 3, 1]
-PROTOCOL_02 = [650, 1, 10, 1, 2, 2, 1]
-PROTOCOL_03 = [100, 30, 71, 4, 11, 9, 6]
-PROTOCOL_04 = [380, 1, 6, 1, 3, 3, 1]
-PROTOCOL_05 = [500, 6, 14, 1, 2, 2, 1]
-PROTOCOL_06 = [200, 1, 10, 1, 5, 1, 1]
-
-PROTOCOLS = [None, PROTOCOL_01, PROTOCOL_02, PROTOCOL_03, PROTOCOL_04, PROTOCOL_05, PROTOCOL_06]
+HEX_CHARS = "1234567890abcdef"
 
 class Wave():
-    def __init__(self, protocol : int, pin : int):
-        self.pin = Pin(pin, Pin.OUT)
-        self.intPin = pin
-        self.pinMode = TX
-        self.protocol = PROTOCOLS[protocol]
+    def __init__(self, UARTid : int,  rxPin : int, txPin : int, baudrate : int = 9600):
+        self.pin = UART(UARTid, tx = Pin(txPin), rx = Pin(rxPin), baudrate = baudrate, bits = 8, parity = None, stop = 2)
 
-    def sendWave(self, Bit : str):
-        if self.pinMode == RX:
-            self.pin = Pin(self.intPin, Pin.OUT)
-            self.pinMode = TX
-            self.syncTx()
-        if str(Bit) == BIT_00:
-            HighPulse = PULSE_HIGH_BIT00 * self.protocol[0]
-            LowPulse = PULSE_LOW_BIT00 * self.protocol[0]
-        elif str(Bit) == BIT_01:
-            HighPulse = PULSE_HIGH_BIT01 * self.protocol[0]
-            LowPulse = PULSE_LOW_BIT01 * self.protocol[0]
+    def sendWave(self, bit : str):
+        if str(bit) == "0":
+            self._send0x0()
+        elif str(bit) == "1":
+            self._send0x1()
         else:
             raise Exception("Wrong bit!")
-        self.pin.high()
-        self.setDelay(HighPulse / DELAY_OPERATOR)
-        self.pin.low()
-        self.setDelay(LowPulse / DELAY_OPERATOR)
 
-    def sendWaves(self, Bits : str):
-        if self.pinMode == RX:
-            self.pin = Pin(self.intPin, Pin.OUT)
-            self.pinMode = TX
-            self.syncTx()
-        for Bit in str(Bits):
-            if Bit == BIT_00:
-                HighPulse = PULSE_HIGH_BIT00 * self.protocol[0]
-                LowPulse = PULSE_LOW_BIT00 * self.protocol[0]
-            elif Bit == BIT_01:
-                HighPulse = PULSE_HIGH_BIT01 * self.protocol[0]
-                LowPulse = PULSE_LOW_BIT01 * self.protocol[0]
+    def sendWaves(self, bits : str):
+        for Bit in str(bits):
+            if str(Bit) == "0":
+                self._send0x0()
+            elif str(Bit) == "1":
+                self._send0x1()
             else:
                 raise Exception("Wrong bit!")
-            self.pin.high()
-            self.setDelay(HighPulse / DELAY_OPERATOR)
-            self.pin.low()
-            self.setDelay(LowPulse / DELAY_OPERATOR)
     
-    def recvWaves(self, length : int = 24):
-        if self.pinMode == TX:
-            self.pin = Pin(self.intPin, Pin.IN, Pin.PULL_DOWN)
-            self.pinMode = RX
-        bits = ""
+    def recvWaves(self, length : int = 0):
         while True:
-            if len(bits) >= length:
-                return bits
-            else:
-                try:
-                    if self.pin.value() != 1:
-                        raise Exception()
-                    self.setDelay(PULSE_HIGH_BIT00 * self.protocol[0] / DELAY_OPERATOR)
-                    if self.pin.value() != 0:
-                        raise Exception()
-                    self.setDelay(PULSE_LOW_BIT00 * self.protocol[0] / DELAY_OPERATOR)
-                    bits += "0"
-                except:
-                    try:
-                        if self.pin.value() != 1:
-                            raise Exception()
-                        self.setDelay(PULSE_HIGH_BIT01 * self.protocol[0] / DELAY_OPERATOR)
-                        if self.pin.value() != 0:
-                            raise Exception()
-                        self.setDelay(PULSE_LOW_BIT01 * self.protocol[0] / DELAY_OPERATOR)
-                        bits += "1"
-                    except:
-                        continue
+            if self.pin.any():
+                if length == 0:
+                    response = self.pin.read()
+                else:
+                    response = self.pin.read(length)
+                if response == None:
+                    pass
+                else:
+                    return getBinaryFromHexBytes(response)
 
     def overflow(self, timeout : int = NO_TIMEOUT, Bit : str = "1"):
-        if self.pinMode == RX:
-            self.pin = Pin(self.intPin, Pin.OUT)
-            self.pinMode = TX
-            self.syncTx()
         if timeout == NO_TIMEOUT:
             while True:
                 self.sendWave(Bit)
@@ -116,26 +56,31 @@ class Wave():
                 if (currentTime - beforeTime) > timeout:
                     break
 
-    def syncTx(self):
-        if self.pinMode == RX:
-            self.pin = Pin(self.intPin, Pin.OUT)
-            self.pinMode = TX
-        self.pin.high()
-        self.setDelay((SYNC_HIGH * self.protocol[0]) / DELAY_OPERATOR)
-        self.pin.low()
-        self.setDelay((SYNC_LOW * self.protocol[0]) / DELAY_OPERATOR)
+    def _send0x0(self):
+        self.pin.write(BIT_00)
 
-    def setDelay(self, delay):
-        end = time.time() + delay - (delay / 100)
-        while time.time() < end:
-            time.sleep(delay / 100)
+    def _send0x1(self):
+        self.pin.write(BIT_01)
 
-def getValidBits(bits : str, counter : int = 3):
+def getBinaryFromHexBytes(hexbytes : bytes):
+    formatted = str(hexbytes)[2:-1].replace("\\x", "").replace(" ", "")
+    newFormatted = ""
+    for hbyte in formatted:
+        if hbyte in HEX_CHARS:   
+            newFormatted += hbyte
+    try:
+        return bin(int(newFormatted, 16))[2:]
+    except:
+        return None
+
+def getValidBits(bits : str, chunksize : int = 8, counter : int = 3):
+        if bits == None:
+            return None
         maxCount = counter - 1
         maxEscape = round(maxCount / 2)
         chunks = []
-        for i in range(1, len(bits), 8):
-            chunks.append(bits[i:i + 8])
+        for i in range(1, len(bits), chunksize):
+            chunks.append(bits[i:i + chunksize])
         if len(chunks) < 1:
             return None
         finalstring = ""
